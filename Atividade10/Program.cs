304aae88cd9ceea3e59c845e25a050d20d3e3298
BernardoLomas <bernardo.lomasb@gmail.com>
2025-09-06 22:06:00 -0300

    Atividade 11

---
+++ Atividade10/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        Random x = new Random();
+        int[] vetor = new int[x.Next(10, 50)];
+        
+
+
+    }
+    
+}
