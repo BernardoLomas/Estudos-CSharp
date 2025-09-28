e9fdaf33fa44c30e991330a7c7c9ba3fc1d4c7fb
BernardoLomas <bernardo.lomasb@gmail.com>
2025-09-27 23:04:06 -0300

    Atividade 1 - Recursividade

---
+++ Recursividade/Atividade1/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Random rand = new Random();
+        int[] vetor = new int[20];
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = rand.Next(1, 101);
+            Console.Write($"{vetor[i]} - ");
+        }
+
+        Console.ReadKey();
+        Console.Clear();
+
+        int soma = SomaRecursiva(vetor, vetor.Length - 1);
+
+        Console.WriteLine($"A soma dos elementos do vetor é: {soma}");
+    }
+
+    static int SomaRecursiva(int[] vetor, int indice)
+    {
+        if (indice < 0) 
+            return 0;
+
+        return vetor[indice] + SomaRecursiva(vetor, indice - 1);
+    }
+}
