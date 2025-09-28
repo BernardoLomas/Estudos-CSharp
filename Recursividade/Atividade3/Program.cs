d3efe65693d14ee1c5cccb29d9e3038a59e2534c
BernardoLomas <bernardo.lomasb@gmail.com>
2025-09-28 00:04:21 -0300

    Atividade 3 - Recursividade

---
+++ Recursividade/Atividade3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string palavra = "bernardo";
+
+        string invertida = InverteString(palavra, palavra.Length - 1);
+
+        Console.WriteLine($"Original: {palavra}");
+        Console.WriteLine($"Invertida: {invertida}");
+    }
+
+    static string InverteString(string palavra, int indice)
+    {
+        // Caso base: chegou no primeiro caractere
+        if (indice == 0)
+            return palavra[0].ToString();
+
+        // Concatena o caractere atual com a chamada recursiva
+        return palavra[indice] + InverteString(palavra, indice - 1);
+    }
+}
