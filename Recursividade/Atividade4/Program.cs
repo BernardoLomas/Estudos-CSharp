8592331e855cbaaa740bcef45a021de6d23734f8
BernardoLomas <bernardo.lomasb@gmail.com>
2025-09-28 19:22:48 -0300

    Atividade 4 - recursividade

---
+++ Recursividade/Atividade4/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        double x, y;
+
+        Console.WriteLine($"Escreva o valor de X: ");
+        x = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Escreva o valor de Y: ");
+        y = double.Parse(Console.ReadLine());
+
+        double potencia = Potencias(x, y);
+
+        Console.WriteLine($"{x} elevado a {y} é igual a: {potencia}");
+
+    }
+
+    static double Potencias(double x, double y)
+    {
+        if (y == 0)
+            return 1;
+            
+        return x * Potencias(x, y - 1);
+    }
+}
