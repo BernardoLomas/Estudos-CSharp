8f6d76496b5d08bbb25899fec786df8ff5ca62e9
BernardoLomas <bernardo.lomasb@gmail.com>
2025-09-28 21:56:00 -0300

    Atividades 8 e 9 - Recursividade

---
+++ Recursividade/Atividade8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite um número: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int soma = SomaDivisores(n, n);
+
+        Console.WriteLine($"A soma dos divisores de {n} é: {soma}");
+    }
+
+    static int SomaDivisores(int n, int divisor)
+    {
+        if (divisor == 0) 
+            return 0;
+
+        if (n % divisor == 0)
+            return divisor + SomaDivisores(n, divisor - 1);
+        else
+            return SomaDivisores(n, divisor - 1);
+    }
+}
