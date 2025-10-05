af1819896d9c3150d59960ecf47a36e838289381
BernardoLomas <bernardo.lomasb@gmail.com>
2025-10-05 19:45:16 -0300

    Atividade 2 TAD feita

--- Recursividade/Atividade1/obj/Debug/net9.0/Atividade1.AssemblyInfo.cs
+++ Recursividade/Atividade1/obj/Debug/net9.0/Atividade1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6d76496b5d08bbb25899fec786df8ff5ca62e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad225587adbd1b288a402c66931960caf0371e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TADeClasses/Atividade2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class DataNascimento
+{
+    public int Dia { get; set; }
+    public int Mes { get; set; }
+    public int Ano { get; set; }
+
+    public DataNascimento(int dia, int mes, int ano)
+    {
+        Dia = dia;
+        Mes = mes;
+        Ano = ano;
+    }
+
+    public override string ToString()
+    {
+        return $"{Dia:D2}/{Mes:D2}/{Ano}";
+    }
+}
+
+class Aluno
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public DataNascimento Nascimento { get; set; }
+    public string Telefone { get; set; }
+
+    public Aluno(string nome, string email, DataNascimento nascimento, string telefone)
+    {
+        Nome = nome;
+        Email = email;
+        Nascimento = nascimento;
+        Telefone = telefone;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {Nome}\nEmail: {Email}\nData de Nascimento: {Nascimento}\nTelefone: {Telefone}\n";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataNascimento data1 = new DataNascimento(25, 01, 2007);
+        Aluno aluno1 = new Aluno("Bernardo Lomas", "bernardo.lomasb@gmail.com", data1, "3199999999");
+        Console.WriteLine(aluno1);
+    }
+}
