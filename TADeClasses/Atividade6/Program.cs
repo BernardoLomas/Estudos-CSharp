4e3e6331d9046aaf28bcfabcadafaf22566b9696
BernardoLomas <bernardo.lomasb@gmail.com>
2025-10-06 15:50:15 -0300

    Estudos para prova AED, atividade 6 de recursividade feita

---
+++ TADeClasses/Atividade6/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int n;
+
+        Console.WriteLine($"Escreva o valor de n: ");
+        n = int.Parse(Console.ReadLine());
+
+        int indice = 1;
+        int soma = SomaN(n, indice);
+        Console.WriteLine($"A soma de todos os elementos de 1 até {n} é igual a: {soma}");
+    }
+
+    static int SomaN(int n, int indice)
+    {
+        if (indice > n)
+            return 0;
+        
+        return indice + SomaN(n, indice + 1);
+    }
+}
