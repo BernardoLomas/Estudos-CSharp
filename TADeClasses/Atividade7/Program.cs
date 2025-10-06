dc00efc76bdf0abfea80226d74f2cb94742b4591
BernardoLomas <bernardo.lomasb@gmail.com>
2025-10-06 16:13:14 -0300

    Atividade 7 feita

--- TADeClasses/Atividade7/Program.cs
+++ TADeClasses/Atividade7/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        int fatorial;
+
+        Console.WriteLine($"Escreva o valor fatorial a ser calculado: ");
+        fatorial = int.Parse(Console.ReadLine());
+
+        int Fcalc = Calculo(fatorial);
+        Console.WriteLine($"O valor de {fatorial} fatorial é: {Fcalc}");
+    }
+
+    static int Calculo(int fatorial)
+    {
+        if (fatorial == 1)
+            return 1;
+        else
+            return fatorial * Calculo(fatorial - 1);
+    }
+}
