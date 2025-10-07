7bc47d135a13581ae4c4db1545e566e29e23832c
BernardoLomas <bernardo.lomasb@gmail.com>
2025-10-07 15:23:48 -0300

    Atividade 9

---
+++ TADeClasses/Atividade9/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int n;
+        Console.WriteLine("Valor de n: ");
+        n = int.Parse(Console.ReadLine());
+
+        ContagemRegressiva(n - 1); 
+    }
+
+    static int ContagemRegressiva(int n)
+    {
+        if (n < 0)
+            return 0;
+
+        Console.WriteLine(n);
+        return ContagemRegressiva(n - 1);
+    }
+}
