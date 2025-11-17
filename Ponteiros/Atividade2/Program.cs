608640a0ba737f61cdaa71989d9f6d251dc79322
BernardoLomas <bernardo.lomasb@gmail.com>
2025-11-16 21:48:43 -0300

    Atividade 3

---
+++ Ponteiros/Atividade2/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Stack<int> PilhaA = new Stack<int>();
+        Stack<int> PilhaB = new Stack<int>();
+        Queue<int> FilaComum = new Queue<int>();
+
+        for (int i = 0; i <= 4; i++) 
+        {
+            Console.WriteLine("Escreva um valor para armazenar na pilha A:");
+            int Valor = int.Parse(Console.ReadLine());
+            PilhaA.Push(Valor);
+        }
+
+        for (int i = 0; i <= 4; i++) 
+        {
+            Console.WriteLine("Escreva um valor para armazenar na pilha B:");
+            int Valor = int.Parse(Console.ReadLine());
+            PilhaB.Push(Valor);
+        }
+
+        while (PilhaA.Count > 0)
+        {
+            int Elemento = PilhaA.Pop();
+
+            if (PilhaB.Contains(Elemento))
+            {
+                FilaComum.Enqueue(Elemento);
+            }
+        }
+
+        Console.WriteLine("Elementos em comum entre as pilhas: ");
+        foreach (var repetidos in FilaComum)
+        {
+            Console.WriteLine(repetidos);
+        }
+    }
+}
---
+++ Ponteiros/Atividade3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        List<int> Inteiros = new List<int>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            int num = rand.Next(1, 21);
+            Inteiros.Add(num);
+        }
+
+        Console.WriteLine("Lista completa: ");
+        foreach (var num in Inteiros)
+        {
+            Console.Write(num + "  ");
+        }
+
+        Stack<int> Pilha = new Stack<int>();
+        Stack<int> PilhaRepetidos = new Stack<int>();
+        List<int> RepetidosNaLista = new List<int>();
+
+        foreach (var num in Inteiros)
+        {
+            if (!RepetidosNaLista.Contains(num))
+            {
+                Pilha.Push(num);
+                RepetidosNaLista.Add(num);
+            }
+            else
+            {
+                if (Pilha.Contains(num))
+                {
+                    Stack<int> Temp = new Stack<int>();
+                    while (Pilha.Count > 0)
+                    {
+                        int Topo = Pilha.Pop();
+                        if (Topo != num)
+                            Temp.Push(Topo);
+                    }
+                    while (Temp.Count > 0)
+                        Pilha.Push(Temp.Pop());
+                }
+
+                if (!PilhaRepetidos.Contains(num))
+                    PilhaRepetidos.Push(num);
+            }
+        }
+
+        Console.WriteLine("\nPilha sem os números repetidos: ");
+        foreach (var p in Pilha)
+            Console.Write(p + " ");
+
+        Console.WriteLine("\nPilha de repetidos: ");
+        foreach (var p in PilhaRepetidos)
+            Console.Write(p + " ");
+    }
+}
