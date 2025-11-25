5170e39a58be22682f6d59b11f46b8577ef83264
BernardoLomas <bernardo.lomasb@gmail.com>
2025-11-25 14:54:59 -0300

    Atividade 14 - Feita

--- Ponteiros/Atividade14/Program.cs
+++ Ponteiros/Atividade14/Program.cs
@@ -1,4 +1,6 @@
-﻿class Navegador
+﻿using System;
+using System.Collections.Generic;
+class Navegador
 {
     public string URL { get; set; }
     public string Titulo { get; set; }
@@ -14,6 +16,70 @@ class Program
 {
     static void Main()
     {
-        
+        Stack<Navegador> back = new Stack<Navegador>();
+        Navegador Atual = null;
+
+        while (true)
+        {
+            Console.WriteLine($"\n === MENU === ");
+            Console.WriteLine($"1 - Visitar nova página");
+            Console.WriteLine($"2 - Voltar (POP)");
+            Console.WriteLine($"3 - Mostrar página atual");
+            Console.WriteLine($"4 - Histórico");
+            Console.WriteLine($"0 - Sair");
+            Console.Write("Opção: ");
+
+            string opcao = Console.ReadLine();
+
+            switch(opcao)
+            {
+                case "1":
+                    Console.Write($"URL: ");
+                    string url = Console.ReadLine();
+                    Console.Write("Título: ");
+                    string titulo = Console.ReadLine();
+
+                    if (Atual != null)
+                        back.Push(Atual); //Push coloca um objeto no topo da pilha, nesse casso se a Página atual não for do tipo null, ela vai para o topo
+                    
+                    Atual = new Navegador(url, titulo);
+                    Console.WriteLine($"Visitando: {Atual.Titulo} | URL: {Atual.URL}");
+                    break;
+                
+                case "2":
+                    if(back.Count == 0)
+                    {
+                        Console.WriteLine($"Não há páginas para voltar!");
+                    }
+                    else
+                    {
+                        Atual = back.Pop();
+                        Console.WriteLine($"Voltando para: {Atual.Titulo}");
+                    }
+                    break;
+                
+                case "3":
+                    if(Atual == null)
+                    {
+                        Console.WriteLine("Nenhuma página aberta!");
+                    }
+                    else
+                        Console.WriteLine($"Página atual: {Atual.Titulo} | URL: {Atual.URL}");
+                    break;
+                
+                case "4":
+                    Console.WriteLine("=== HISTÓRICO ===");
+                    foreach(Navegador p in back)
+                        Console.WriteLine($"{p.Titulo} | URL: {p.URL}");
+                    break;
+                
+                case "0":
+                    return;
+
+                default:
+                    Console.WriteLine("Opção inválida!!");
+                    break;
+            }
+        }
     }
 }
