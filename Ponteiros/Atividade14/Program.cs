119bc02d9c0d919afe081b4493ae7a0365da0d02
BernardoLomas <bernardo.lomasb@gmail.com>
2025-11-25 11:49:44 -0300

    Atividade14

---
+++ Ponteiros/Atividade14/Program.cs
@@ -0,0 +1,19 @@
+﻿class Navegador
+{
+    public string URL { get; set; }
+    public string Titulo { get; set; }
+
+    public Navegador(string url, string titulo)
+    {
+        URL = url;
+        Titulo = titulo;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
