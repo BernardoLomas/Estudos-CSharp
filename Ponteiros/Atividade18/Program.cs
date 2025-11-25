82d118c17b2397f9c1ea32c64ef4e007d530a5fc
BernardoLomas <bernardo.lomasb@gmail.com>
2025-11-25 16:42:47 -0300

    Atividade 18 - Feita

---
+++ Ponteiros/Atividade18/Program.cs
@@ -0,0 +1,63 @@
+﻿class Produtos
+{
+    public string Nome { get; set; }
+    public double Preco { get; set;}
+    public Produtos Proximo { get; set;}
+
+    public Produtos(string nome, double preco)
+    {
+        Nome = nome;
+        Preco = preco;
+        Proximo = null;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Quantos produtos deseja inserir? ");
+        int quantidade = int.Parse(Console.ReadLine());
+        
+        Produtos Primeiro = null;
+        Produtos Ultimo = null;
+
+        for(int i = 0; i < quantidade; i++)
+        {
+            Console.WriteLine($"Escreva o nome do produto: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            Produtos novo = new Produtos(nome, preco);
+
+            if (Primeiro == null)
+            {
+                Primeiro = novo;
+                Ultimo = novo;
+            }
+            else
+            {
+                // Caso 2: Inserir no começo (preço > 100)
+                if (preco > 100)
+                {
+                    novo.Proximo = Primeiro;
+                    Primeiro = novo;
+                }
+                else // Caso 3: Inserir no final (preço <= 100)
+                {
+                    Ultimo.Proximo = novo;
+                    Ultimo = novo;
+                }
+            }
+        }
+
+        Produtos Atual = Primeiro;
+        while(Atual != null)
+        {
+            Console.WriteLine($"Nome do produto: {Atual.Nome} | Preço do produto: {Atual.Preco}");
+            Atual = Atual.Proximo;
+        }
+    }
+}
--- Ponteiros/Atividade18/obj/Debug/net8.0/Atividade18.AssemblyInfo.cs
+++ Ponteiros/Atividade18/obj/Debug/net8.0/Atividade18.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Atividade17")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Atividade18")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4061e294144e8e1da26d2d4e1cc27754a5cdfbaa")]
-[assembly: System.Reflection.AssemblyProductAttribute("Atividade17")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Atividade17")]
+[assembly: System.Reflection.AssemblyProductAttribute("Atividade18")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Atividade18")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
