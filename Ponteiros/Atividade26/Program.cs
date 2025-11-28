55713cdf3822d052631034409038de1545e960b8
BernardoLomas <bernardo.lomasb@gmail.com>
2025-11-28 12:17:00 -0300

    Atividade 26

--- Ponteiros/Atividade26/Program.cs
+++ Ponteiros/Atividade26/Program.cs
@@ -1,1 +1,61 @@
-﻿
+﻿class Musica
+{
+    public string Titulo {get; set;}
+    public string Artista {get; set;}
+    public double Duração {get; set;}
+    public Musica Proximo {get; set;}
+
+    public Musica(string titulo, string artista, double duracao)
+    {
+        Titulo = titulo;
+        Artista = artista;
+        Duração = duracao;
+        Proximo = null;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Quantas faixas deseja tocar na playlist? ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        Musica Primeiro = null;
+        Musica Ultimo = null;
+
+        for(int i = 0; i < quantidade; i++)
+        {
+            Console.WriteLine($"Nome da faixa: ");
+            string titulo = Console.ReadLine();
+            Console.WriteLine($"Artista: ");
+            string artista = Console.ReadLine();
+            Console.WriteLine($"Duração da faixa:  ");
+            double duracao = double.Parse(Console.ReadLine());
+
+            Musica novo = new Musica(titulo, artista, duracao);
+
+            if(Ultimo == null)
+            {
+                Ultimo = novo;
+                Ultimo.Proximo = Ultimo;
+            }
+            else
+            {
+                novo.Proximo = Ultimo.Proximo;
+                Ultimo.Proximo = novo;
+                Ultimo = novo;
+            }
+        }
+
+        Musica Atual = Ultimo;
+        int cont = 0;
+
+        do
+        {
+            Console.WriteLine($"Nome da música: {Atual.Titulo} | Artista: {Atual.Artista} | Duração: {Atual.Duração}");
+            Atual = Ultimo.Proximo;
+            cont++;
+
+        } while(cont < 11);
+    }
+}
