9a41fae83f2a8134e350984ed94880bcd94f9e4e
BernardoLomas <bernardo.lomasb@gmail.com>
2025-11-29 19:07:31 -0300

    Atividade 29 - Feita

---
+++ Ponteiros/Atividade29/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Quantidade de nomes para serem lidos: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        Stack<string> PilhaNomes = new Stack<string>();
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            Console.WriteLine($"Escreva o nome {i + 1}: ");
+            string nome = Console.ReadLine();
+            PilhaNomes.Push(nome);
+        }
+
+
+        for (int j = PilhaNomes.Count; j > 0; j--)
+        {
+            if (j > 1)
+            {
+                PilhaNomes.Pop();
+            }
+            else
+                Console.WriteLine($"Último nome da lista: {PilhaNomes.Peek()}");
+        }
+    }
+}
