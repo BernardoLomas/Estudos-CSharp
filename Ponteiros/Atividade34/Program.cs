3428a2be1b8f89cc34175f7c245a2a799d0fe5e8
BernardoLomas <bernardo.lomasb@gmail.com>
2025-12-09 10:20:41 -0300

    Atividade 34 ponteiros

---
+++ Ponteiros/Atividade34/Program.cs
@@ -0,0 +1,51 @@
+﻿class Numero
+{
+    public int Num {get; set;}
+    public Numero Proximo { get; set; }
+    public Numero Anterior { get; set; }
+
+    public Numero(int numero)
+    {
+        Num = numero;
+        Proximo = null;
+        Anterior = null;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine($"Escreva a quantidade de números na lista: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        Numero Primeiro = null;
+        Numero Ultimo = null;
+
+        for(int i = 0; i < quantidade; i++)
+        {
+            Console.WriteLine($"Número {i + 1}/{quantidade}:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Numero novo = new Numero(numero);
+
+            if(Primeiro == null)
+            {
+                Primeiro = novo;
+                Ultimo = novo;
+            }
+            else
+            {
+                novo.Proximo = Primeiro;
+                Primeiro.Anterior = novo;
+                Primeiro = novo;
+            }
+        }
+
+        Numero Atual = Primeiro;
+        while(Atual != null)
+        {
+            Console.WriteLine($"Número: {Atual.Num}");
+            Atual = Atual.Proximo;
+        }
+    }
+}
