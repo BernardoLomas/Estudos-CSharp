8be24983f6f8b21e3224c1720aabe6bb2be3891b
BernardoLomas <bernardo.lomasb@gmail.com>
2025-12-09 09:47:23 -0300

    Atividade 32 ponteiros

--- Ponteiros/Atividade32/Program.cs
+++ Ponteiros/Atividade32/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Objeto
+{
+    public string Nome { get; set; }
+    public Objeto Proximo { get; set; }
+    public Objeto Anterior { get; set; }
+
+    public Objeto(string nome)
+    {
+        Nome = nome;
+        Proximo = null;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine($"Quantidade de nomes: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        Objeto Primeiro = null;
+        Objeto Ultimo = null;
+
+        for(int i = 0; i < quantidade; i++)
+        {
+            Console.WriteLine($"Nome {i+1}: ");
+            string nome = Console.ReadLine();
+            Objeto novo = new Objeto(nome);
+
+            if(Primeiro == null)
+            {
+                Primeiro = novo;
+                Ultimo = novo;
+            }
+            else
+            {
+                Ultimo.Proximo = novo;
+                Ultimo = novo;
+            }
+        }
+
+        Objeto Atual = Primeiro;
+
+        while(Atual != null)
+        {
+            Console.WriteLine($"Nome - {Atual.Nome}");
+            Atual = Atual.Proximo;
+        }
+    }
+}
